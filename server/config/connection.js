336d85b7ae41fe1d094fd86629f5a9f916a56b2d
sallaball <sallaball1206@gmail.com>
2022-10-22 12:51:02 -0500

    installed bcryptjs

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/book-search-engine', {
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/book-search-engine', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
+
+
